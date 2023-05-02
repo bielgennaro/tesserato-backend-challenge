7af52231dd6dd5fa4f70f5d14d7d8c1707b6a027
bielgennaro <zirondigabriel@gmail.com>
2023-05-02 05:31:46 -0300

    implementando responses e parametros

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,14 +3,15 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async create({ request }: HttpContextContract) {
-    const { name, isEnabled, companyId } = request.all()
-
-    const company = await User.create({ name, isEnabled, companyId })
-    return company
+  public async create({ request, response }: HttpContextContract) {
+    const { id, name, isEnabled, companyId} = request.body()
+    
+    //! como eu vou consertar essa coisa...
+    const user = await User.create({ id, name, isEnabled, companyId})
+    return response.status(200).json(user)
   }
-  public async update() {
-    const all = await User.all()
-    return all
+  public async list({ response, params }: HttpContextContract) {
+    const users = await User.find(params.id)
+    return response.status(200).json(users)
   }
 }
