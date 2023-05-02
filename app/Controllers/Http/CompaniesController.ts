353d6b89af9dab85d7fb0cd7efd6bf3d42ec9816
bielgennaro <zirondigabriel@gmail.com>
2023-05-02 05:31:06 -0300

    implementação de  responses

--- app/Controllers/Http/CompaniesController.ts
+++ app/Controllers/Http/CompaniesController.ts
@@ -4,22 +4,15 @@ import Company from 'App/Models/Company'
 import User from 'App/Models/User'
 
 export default class CompanyController {
-  public async list({ response }: HttpContextContract) {
-    const companies = await Company.all()
+  public async create({ request, response }: HttpContextContract) {
+    const { id, name, isEnabled} = request.body()
+
+    const companies = await User.create({ id, name, isEnabled})
     return response.status(200).json(companies)
   }
 
-  public async create({ request, response }: HttpContextContract) {
-    const { name, users } = request.only(['name', 'users'])
-    const company = new Company()
-    company.name = name
-    await company.save()
-
-    if (users && users.length > 0) {
-      const associatedUsers = await User.query().whereIn('id', users)
-      await company.related('users').createMany(associatedUsers)
-    }
-
-    return response.status(201).json(company)
+  public async list({ response, params }: HttpContextContract) {
+    const companies = await Company.find(params.id)
+    return response.status(201).json(companies)
   }
 }
