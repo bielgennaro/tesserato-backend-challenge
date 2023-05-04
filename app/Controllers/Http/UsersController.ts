000be6bf4cd0688ca79dfc36df593b0ed04db1d6
bielgennaro <zirondigabriel@gmail.com>
2023-05-04 08:47:36 -0300

    consertando companyId :)

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -5,8 +5,7 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async create({ request, response }: HttpContextContract) {
     const { id, name, isEnabled, companyId} = request.body()
-    
-    //! como eu vou consertar essa coisa...
+  
     const user = await User.create({ id, name, isEnabled, companyId})
     return response.status(200).json(user)
   }
